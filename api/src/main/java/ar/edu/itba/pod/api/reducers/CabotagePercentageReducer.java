b61bec54f7641211f4ca6a9d4202173ca606cfb8
Martin Grabina <mgrabina@itba.edu.ar>
2019-10-20 11:54:41 -0300

    Fixed Query 2

--- api/src/main/java/ar/edu/itba/pod/api/reducers/CabotagePercentageReducer.java
+++ api/src/main/java/ar/edu/itba/pod/api/reducers/CabotagePercentageReducer.java
@@ -3,36 +3,38 @@ package ar.edu.itba.pod.api.reducers;
 import com.hazelcast.mapreduce.Reducer;
 import com.hazelcast.mapreduce.ReducerFactory;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CabotagePercentageReducer implements ReducerFactory<String, Boolean, Double> {
     private static final long serialVersionUID = -4213292631235182815L;
 
-    private int total;
+    private AtomicInteger total;
 
     public CabotagePercentageReducer() {
-        total = 0;
+        total = new AtomicInteger(0);
     }
 
     @Override
     public Reducer<Boolean, Double> newReducer(final String airline) {
         return new Reducer<Boolean, Double>() {
-            private int cabotage;
+            private AtomicInteger cabotage;
 
             @Override
             public void beginReduce() {
-                cabotage = 0;
+                cabotage = new AtomicInteger(0);
             }
 
             @Override
             public void reduce(Boolean isCabotage) {
                 if (isCabotage){
-                    cabotage++;
-                    total++;
+                    cabotage.incrementAndGet();
+                    total.incrementAndGet();
                 }
             }
 
             @Override
             public Double finalizeReduce() {
-                return (double) cabotage / (double)total;
+                return (double) cabotage.get() / (double)total.get();
             }
         };
     }
